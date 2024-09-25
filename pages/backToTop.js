fb05265056ea7f7fdcddd4ee08fad11b077e3d47
Furkan İnce <f.ince94@hotmail.com>
2024-09-25 20:31:54 +0300

    sss

--- pages/backToTop.js
+++ pages/backToTop.js
@@ -24,7 +24,7 @@ const JupiterWidget = () => {
         window.Jupiter.init({
           displayMode: "integrated",
           integratedTargetId: "integrated-terminal",
-          endpoint: "https://api.mainnet-beta.solana.com",
+          endpoint: "https://misty-flashy-bridge.solana-mainnet.quiknode.pro",
           strictTokenList: false,
           defaultExplorer: "Solscan",
           formProps: {
