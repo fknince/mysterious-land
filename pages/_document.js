fef367caf5e61c0f99c0aa32213ee572169173f6
Furkan İnce <f.ince94@hotmail.com>
2024-09-25 21:28:51 +0300

    sss

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <script src="https://terminal.jup.ag/main-v1.js" data-preload />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/backToTop.js
+++ pages/backToTop.js
@@ -1,50 +1,74 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const JupiterWidget = () => {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-  const widgetRef = useRef(null);
-  const buttonRef = useRef(null);
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (widgetRef.current && !widgetRef.current.contains(event.target) && !buttonRef.current.contains(event.target)) {
+    const loadScript = () => {
+      const script = document.createElement("script");
+      script.src = "https://terminal.jup.ag/main-v3.js";
+      script.async = true;
+      script.onload = () => {
+        setIsScriptLoaded(true);
+      };
+      document.body.appendChild(script);
+    };
+
+    if (!isScriptLoaded) {
+      loadScript();
+    }
+
+    // Add back button listener for mobile devices
+    const handleBackButton = (event) => {
+      if (isWidgetOpen) {
+        event.preventDefault();
         setIsWidgetOpen(false);
       }
     };
 
-    if (isWidgetOpen) {
+    window.addEventListener('popstate', handleBackButton);
+
+    return () => window.removeEventListener('popstate', handleBackButton);
+  }, [isScriptLoaded, isWidgetOpen]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const widgetContainer = document.querySelector(".widget-container");
+      const widgetButton = document.querySelector(".jupiter-widget-button");
+      if (
+        widgetContainer &&
+        !widgetContainer.contains(event.target) &&
+        !widgetButton.contains(event.target)
+      ) {
+        setIsWidgetOpen(false);
+      }
+    };
+
+    if (isWidgetOpen && isScriptLoaded && window.Jupiter) {
+      window.Jupiter.init({
+        displayMode: "integrated",
+        integratedTargetId: "integrated-terminal",
+        endpoint:
+          "https://cosmopolitan-thrumming-dawn.solana-mainnet.quiknode.pro/1cbcf6878394dbd7fab3a3c65d77f24a6c5bf0c8/",
+        strictTokenList: false,
+        defaultExplorer: "Solscan",
+        formProps: {
+          fixedOutputMint: true,
+          swapMode: "ExactIn",
+          initialAmount: "1000",
+          initialOutputMint: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
+          initialSlippageBps: 5,
+        },
+      });
+
       document.addEventListener("mousedown", handleClickOutside);
-
-      // Load the Jupiter script dynamically when the widget is open
-      const script = document.createElement('script');
-      script.src = 'https://terminal.jup.ag/main-v3.js';
-      script.async = true;
-      script.onload = () => {
-        // Initialize the Jupiter widget after the script has loaded
-        window.Jupiter.init({
-          displayMode: "integrated",
-          integratedTargetId: "integrated-terminal",
-          endpoint: "https://cosmopolitan-thrumming-dawn.solana-mainnet.quiknode.pro/1cbcf6878394dbd7fab3a3c65d77f24a6c5bf0c8/",
-          strictTokenList: false,
-          defaultExplorer: "Solscan",
-          formProps: {
-            fixedOutputMint: true,
-            swapMode: "ExactIn",
-            initialAmount: "1000",
-            initialOutputMint: "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
-            initialSlippageBps: 5,
-          },
-        });
-      };
-
-      document.body.appendChild(script);
-
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-        document.body.removeChild(script); // Cleanup the script when the widget is closed
-      };
     }
-  }, [isWidgetOpen]);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isWidgetOpen, isScriptLoaded]);
 
   const toggleWidget = () => {
     setIsWidgetOpen(!isWidgetOpen);
@@ -52,22 +76,31 @@ const JupiterWidget = () => {
 
   return (
     <>
-      <button 
-        ref={buttonRef}
-        className="jupiter-widget-button" 
-        onClick={toggleWidget}
-      >
-        <img src="/images/jupiter/jupiter-logo.svg" alt="Jupiter" width="46" height="46" />
+      <button className="jupiter-widget-button" onClick={toggleWidget}>
+        <img
+          src="/images/jupiter/jupiter-logo.svg"
+          alt="Jupiter"
+          width="46"
+          height="46"
+        />
       </button>
 
       {isWidgetOpen && (
         <div className="widget-overlay">
-          <div className="widget-container" ref={widgetRef}>
-            <button className="widget-close-button" onClick={() => setIsWidgetOpen(false)}>
-              X
+          <div className="widget-container">
+            <button className="close-button" onClick={() => setIsWidgetOpen(false)}>
+              &times;
             </button>
-
-            <div id="integrated-terminal" style={{alignItems:"center",justifyContent:"center",display:"flex", width: "100%", height: "100%",minHeight:"440px",minWidth:"590px" }}></div>
+            <div
+              id="integrated-terminal"
+              style={{
+                width: "100%",
+                height: "100%",
+                minHeight: "440px",
+                minWidth: "100px",
+                alignContent: "center",
+              }}
+            ></div>
           </div>
         </div>
       )}
@@ -108,40 +141,79 @@ const JupiterWidget = () => {
 
         .widget-container {
           position: relative;
-            background: linear-gradient(45deg, #3f7489, #071020);
-          padding: 5px;
+          background: linear-gradient(45deg, #3f7489, #071020);
+          padding: 20px;
           border-radius: 10px;
           box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
-          width: 450px;
-          height: 600px;
-          max-width: 90%;
+          width: 90%;
+          max-width: 750px;
+          height: 90%;
+          max-height: 700px;
+          display: flex;
           justify-content: center;
-          margin: 0;
-          
+          align-items: center;
+          overflow: hidden;
         }
 
-        .widget-close-button {
+        .close-button {
           position: absolute;
           top: 10px;
           right: 10px;
-          background: none;
+          background: transparent;
           border: none;
-          font-size: 20px;
-          cursor: pointer;
           color: white;
-        }
-
-        .widget-content {
-          margin-top: 40px;
+          font-size: 24px;
+          cursor: pointer;
+          z-index: 10002;
         }
 
         @keyframes fadeIn {
-          from { opacity: 0; }
-          to { opacity: 1; }
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
         }
 
         .widget-overlay {
           animation: fadeIn 0.3s ease;
+        }
+
+        /* Increase font size for span elements inside the widget */
+        .widget-container :global(span) {
+          font-size: 12px !important;
+        }
+
+        @media (max-width: 768px) {
+          .widget-container {
+            width: 95%;
+            height: 95%;
+            padding: 10px;
+          }
+
+          .widget-container :global(span) {
+            font-size: 14px !important;
+          }
+        }
+
+        @media (max-width: 480px) {
+          .widget-container {
+            width: 100%;
+            height: 100%;
+            border-radius: 0;
+          }
+
+          .jupiter-widget-button {
+            right: 15px;
+            bottom: 15px;
+            width: 40px;
+            height: 40px;
+          }
+
+          .widget-container :global(span) {
+            font-size: 12px !important;
+          }
         }
       `}</style>
     </>
